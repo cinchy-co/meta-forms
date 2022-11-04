34810859fd3acd34856eb42bae44cae7ffb63e97
Alvin Ho <alvin.ho@cinchy.com>
2022-11-04 14:42:47 -0400

    cleanup rich text directive

--- src/app/dynamic-forms/fields/cinchy-form-richtext.directive.ts
+++ src/app/dynamic-forms/fields/cinchy-form-richtext.directive.ts
@@ -1,16 +1,10 @@
 import {Component, Input, Output, EventEmitter, ViewChild, AfterViewInit, OnInit,OnDestroy} from '@angular/core';
 import {IEventCallback, EventCallback} from '../models/cinchy-event-callback.model';
 import {ResponseType} from '../enums/response-type.enum';
-import { ImageType } from '../enums/imageurl-type';
-import { faAlignLeft } from '@fortawesome/free-solid-svg-icons';
-// import { Editor } from 'ngx-editor';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
+import {faAlignLeft} from '@fortawesome/free-solid-svg-icons';
+import {AngularEditorConfig} from '@kolkov/angular-editor';
 
-//#region Cinchy Dynamic TextArea
-/**
- * This section is used to create dynamic textarea fields for the cinchy.
- */
-//#endregion
+
 @Component({
   selector: 'cinchy-richtext',
   template: `
@@ -33,35 +27,27 @@ import { AngularEditorConfig } from '@kolkov/angular-editor';
           info
         </mat-icon>
       </div>
-      <ng-container *ngIf="!field.cinchyColumn.isViewOnly && !isDisabled && field.cinchyColumn.canEdit">
-        <angular-editor [placeholder]="'Enter text here...'" [(ngModel)]="field.value" [config]="editorConfig" (blur)="callbackEvent(targetTableName, field.cinchyColumn.name, $event, 'value')"></angular-editor>
-        <mat-error
-          *ngIf="showError && (field.cinchyColumn.isMandatory == true &&(field.value =='' || field.value == null))">
-          *{{field.label}} is Required.
-        </mat-error>
-      </ng-container>
+      <angular-editor [placeholder]="'Enter text here...'" [(ngModel)]="field.value" [config]="editorConfig" (blur)="callbackEvent(targetTableName, field.cinchyColumn.name, $event, 'value')"></angular-editor>
+      <mat-error
+        *ngIf="showError && (field.cinchyColumn.isMandatory == true &&(field.value =='' || field.value == null))">
+        *{{field.label}} is Required.
+      </mat-error>
     </div>
   `,
 })
 export class RichTextDirective implements AfterViewInit, OnInit, OnDestroy {
   @Input() field: any;
-
+  @Input() targetTableName: string;
+  @Input() isDisabled: boolean;
   @Input('fieldsWithErrors') set fieldsWithErrors(errorFields: any) {
     this.showError = errorFields ? !!errorFields.find(item => item == this.field.label) : false;
   };
-
-  @Input() targetTableName: string;
-  @Input() isDisabled: boolean;
+  
   @Output() eventHandler = new EventEmitter<any>();
-  //@ViewChild('editor') editor;
-  isFormatted;
+
   showError: boolean;
-  showImage: boolean;
-  showLinkUrl: boolean;
-  showActualField: boolean;
   faAlignLeft = faAlignLeft;
 
-  //editor: Editor;
   editorConfig: AngularEditorConfig;
   readonly: boolean;
   
@@ -69,60 +55,26 @@ export class RichTextDirective implements AfterViewInit, OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    if (this.field.cinchyColumn.dataFormatType === 'JSON') {
-      this.field.value = JSON.stringify(JSON.parse(this.field.value), null, 2)
-    }
-    this.isFormatted = !!this.field.cinchyColumn.dataFormatType && !this.field.cinchyColumn.dataFormatType?.startsWith(ImageType.default) && this.field.cinchyColumn.dataFormatType !== 'LinkUrl';
-    this.showImage = this.field.cinchyColumn.dataFormatType?.startsWith(ImageType.default);
-    this.showLinkUrl = this.field.cinchyColumn.dataFormatType === 'LinkUrl';
-    this.showActualField = !this.showImage && !this.showLinkUrl;
-
-    
-    this.readonly = this.field.cinchyColumn.canEdit === false || this.field.cinchyColumn.isViewOnly || this.isDisabled;
-    this.editorConfig = {
-      editable: !this.readonly,
-      spellcheck: true,
-      height: 'auto',
-      minHeight: '500px',
-      maxHeight: 'auto',
-      width: 'auto',
-      minWidth: '0',
-      translate: 'yes',
-      enableToolbar: true,
-      showToolbar: true,
-      placeholder: 'Enter text here...',
-      defaultParagraphSeparator: '',
-      defaultFontName: '',
-      defaultFontSize: '',
-      fonts: [
-        {class: 'arial', name: 'Arial'},
-        {class: 'times-new-roman', name: 'Times New Roman'},
-        {class: 'calibri', name: 'Calibri'},
-        {class: 'comic-sans-ms', name: 'Comic Sans MS'}
-      ],
-      customClasses: [],
-      sanitize: true,
-      toolbarPosition: 'top',
-      toolbarHiddenButtons: [
-        ['bold', 'italic'],
-        ['fontSize']
-      ]
-    };
+    this.initEditorConfig();
   }
 
   ngAfterViewInit() {
+    this.initEditorConfig();
+  }
+
+  initEditorConfig(): void {
     this.readonly = this.field.cinchyColumn.canEdit === false || this.field.cinchyColumn.isViewOnly || this.isDisabled;
     this.editorConfig = {
       editable: !this.readonly,
       spellcheck: true,
       height: 'auto',
-      minHeight: '500px',
+      minHeight: '450px',
       maxHeight: 'auto',
       width: 'auto',
       minWidth: '0',
       translate: 'yes',
-      enableToolbar: true,
-      showToolbar: true,
+      enableToolbar: !this.readonly,
+      showToolbar: !this.readonly,
       placeholder: 'Enter text here...',
       defaultParagraphSeparator: '',
       defaultFontName: '',
@@ -134,49 +86,13 @@ export class RichTextDirective implements AfterViewInit, OnInit, OnDestroy {
         {class: 'comic-sans-ms', name: 'Comic Sans MS'}
       ],
       customClasses: [],
-      sanitize: true,
+      sanitize: false,
       toolbarPosition: 'top',
       toolbarHiddenButtons: [
         ['bold', 'italic'],
         ['fontSize']
       ]
     };
-    // if (this.isFormatted) {
-    //   this.editor.getEditor().setOptions({
-    //     showLineNumbers: true,
-    //     tabSize: 4,
-    //     theme: 'ace/theme/sqlserver',
-    //     enableBasicAutocompletion: true,
-    //     enableSnippets: true,
-    //     enableLiveAutocompletion: true,
-    //     highlightGutterLine: true
-    //   });
-    //   switch (this.field.cinchyColumn.dataFormatType) {
-    //     case 'XML':
-    //       this.editor.mode = 'xml';
-    //       break;
-    //     case 'Javascript':
-    //       this.editor.mode = 'javascript';
-    //       break;
-    //     case 'CQL':
-    //       this.editor.mode = 'sqlserver';
-    //       break;
-    //     default:
-    //       this.editor.mode = this.field.cinchyColumn.dataFormatType.toLowerCase();
-    //   }
-    //   this.editor.value = this.field.value;
-      //if (this.field.cinchyColumn.canEdit === false || this.field.cinchyColumn.isViewOnly || this.isDisabled) {
-        //this.editor.setReadOnly(true);
-        
-      //}
-      // this.editor.getEditor().commands.addCommand({
-      //   name: "showOtherCompletions",
-      //   bindKey: "Ctrl-.",
-      //   exec: function (editor) {
-
-      //   }
-      // })
-    //}
   }
 
   ngOnDestroy(): void {
